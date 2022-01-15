717c00c4d26a0fda40d014f2a82765722983d011
Saugata Paul <saugatapaul007@gmail.com>
2022-01-15 13:18:30 -0600

    Added fully charged notification

--- battery.py
+++ battery.py
@@ -9,17 +9,25 @@ battery = psutil.sensors_battery()
 plugged = battery.power_plugged
 percent = battery.percent   
 
+charging = False
+charged = False
+
 #description of code
 if __name__=="__main__": 
 	if not plugged:
-		if percent < 10:
-			notification.notify(
-				title="Warning! Low battery info",
-				message="Less than 10% charge, plug in now"
-			)
-
-		elif percent < 20:
+		if percent < 30:
 			notification.notify( 
 				title="Battery info",
-				message="Less than 20% charge, plug in soon"
+				message="Low charge, plug in soon"
 			)
+	else:
+		if percent < 100:
+			charging = True
+		if percent == 100:
+			charged = True
+			if (charging and charged):
+				notification.notify( 
+				title="Battery info",
+				message="Battery fully charged"
+			)
+			charging = False
